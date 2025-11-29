41e79e82ed479fcd8ad76b9d74099345ccdd9ffe
Jeison <jeison5400@gmail.com>
2025-11-29 14:47:20 -0500

    feat: add Jest configuration and setup files for testing environment, including mocks for Expo and additional matchers for React Native Testing Library.

---
+++ jest.setup.after.js
@@ -0,0 +1,4 @@
+// Importar matchers adicionales de React Native Testing Library
+// Esto debe estar en setupFilesAfterEnv porque necesita que expect esté disponible
+require('@testing-library/jest-native/extend-expect');
+
